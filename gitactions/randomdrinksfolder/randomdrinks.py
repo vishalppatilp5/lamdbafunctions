ed0c53a459c9cf0b7c1c371212e135f70c517594
Vishal Patil <vishal@Vishals-MacBook-Pro.local>
2024-05-19 10:41:34 +0530

    workflows

--- gitactions/randomdrinksfolder/randomdrinks.py
+++ gitactions/randomdrinksfolder/randomdrinks.py
@@ -1,16 +1,20 @@
 import json
-import random
+# import random
 
-def random_drink():
-    drinks = ['coffee','tea','beer', 'wine', 'milk', 'chai latte']
-    return random.choice(drinks)
+# def random_drink():
+#     drinks = ['coffee','tea','beer', 'wine', 'milk', 'chai latte']
+#     return random.choice(drinks)
 
 def handler(event, contect):
-    drink = random_drink()
-    message = f"you should drinksome {drink}"
+    # drink = random_drink()
+    # message = f"you should drinksome {drink}"
+    response_body = {
+        "message" : "Hello World",
+        "Version" : "1.0.0"
+    }
 
     return {
         'statusCode' : 200,
-        'body' : json.dumps({"message" : message , "drink" : drink})
+        'body' : response_body
 
     }
