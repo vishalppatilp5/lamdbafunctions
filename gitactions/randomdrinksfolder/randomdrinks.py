7d72e1f8cc5f6edb7e7d76250cfb8a77d398b73a
Vishal Patil <vishal@Vishals-MacBook-Pro.local>
2024-05-19 20:05:56 +0530

    changes in pythoncode removed path

--- gitactions/randomdrinksfolder/randomdrinks.py
+++ gitactions/randomdrinksfolder/randomdrinks.py
@@ -4,9 +4,9 @@ import boto3
 
 def handler(event, context):
     # Raw event data.
-    path = event["rawPath"]
-    if path != "/":
-        return {"statusCode": 404, "body": "Not found."}
+    # path = event["rawPath"]
+    # if path != "/":
+    #     return {"statusCode": 404, "body": "Not found."}
 
     # Get a reference to the DDB table.
     dynamodb = boto3.resource("dynamodb")
